433aebfa95439d85f0a7919520f81e3b07f5ac8b
Garret James <garretjames@gmail.com>
2018-08-21 03:44:09 -0700

    Finished table CSS and added a popup modal for the currency conversion

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    color: #A7A1AE;
+    margin-top: 10px;
   }
 </style>
--- src/components/CoinComponent.vue
+++ src/components/CoinComponent.vue
@@ -3,47 +3,54 @@
     <div class="row">
       <div class="col-md-8">
         <div class="example">
-          <table class="table">
+          <table class="crypto-table">
             <thead>
             <tr>
-              <th>Name</th>
-              <th>Symbol</th>
-              <th>Price (USD)</th>
-              <th>7 Day Change (%)</th>
+              <th><h1>#</h1></th>
+              <th><h1>Name</h1></th>
+              <th><h1>Symbol</h1></th>
+              <th><h1>Price (USD)</h1></th>
+              <th><h1>7 Day Change (%)</h1></th>
+              <th><h1>Conversion</h1></th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="coin in coins" v-bind:key="coin.id">
+              <td>{{ coin.rank }}</td>
               <td>{{ coin.name }}</td>
               <td>{{ coin.symbol }}</td>
               <td>{{ coin.price_usd }}</td>
               <td>{{ coin.percent_change_7d }}</td>
+              <td v-on:click="show(coin.name)"><img width="35" height="35" src="../assets/convertImg.png" /></td>
             </tr>
             </tbody>
           </table>
         </div>
       </div>
       <div class="col-md-4">
-        <div class="example">
-          <form>
-            <div class="form-group">
-              <label for="usd">Amount (USD)</label>
-              <input type="text" class="form-control" v-model="input.amount" id="usd" placeholder="Amount (USD)">
-            </div>
-            <div class="form-group">
-              <label>
-                <select v-model="input.cryptocurrency">
-                  <option v-for="coin in coins" v-bind:key="coin.id">{{ coin.name }}</option>
-                </select>
-              </label>
-            </div>
-            <button type="button" class="btn btn-default" v-on:click="convert()">Convert</button>
-          </form>
-          <br />
-          <p>
-            <strong>Value:</strong> {{ specific_coin_amount }}
-          </p>
-        </div>
+        <modal @before-open="beforeOpen" classes="conversion-form" width="300" height="260" name="conversion-form">
+          <div>
+            <form>
+              <div class="form-group">
+                <label for="usd">Amount (USD)</label>
+                <input type="text" class="form-control" v-model="input.amount" id="usd" placeholder="Amount (USD)">
+              </div>
+              <div class="form-group">
+                <label>
+                  <select v-model="coinName">
+                    <option v-for="coin in coins" v-bind:key="coin.id">{{ coin.name }}</option>
+                  </select>
+                </label>
+              </div>
+              <button type="button" class="btn btn-default" v-on:click="convert()">Convert</button>
+            </form>
+            <br />
+            <p>
+              <strong>Value:</strong> {{ specific_coin_amount }}
+            </p>
+          </div>
+        </modal>
+
       </div>
     </div>
   </div>
@@ -63,12 +70,13 @@ export default {
       },
       specific_coin_amount: 0,
       timer: '',
+      coinName: '',
       i: 5
     }
   },
   created: function () {
     this.fetchCoinInfo()
-    this.timer = setInterval(this.fetchCoinInfo, 10000)
+    setInterval(this.fetchCoinInfo, 10000)
   },
   // TODO check what the mounted function does
   /* mounted() {
@@ -80,12 +88,22 @@ export default {
   }, */
   methods: {
     fetchCoinInfo: function () {
-      this.i++
-      axios({ method: 'GET', 'url': 'https://api.coinmarketcap.com/v1/ticker/?limit=' + this.i }).then(result => {
+      // this.i++
+      axios({ method: 'GET', 'url': 'https://api.coinmarketcap.com/v1/ticker/?limit=15' }).then(result => {
         this.coins = result.data
       }, error => {
         console.error(error)
       })
+    },
+    show (coinName) {
+      this.$modal.show('conversion-form', {coinName: coinName})
+    },
+    hide () {
+      this.$modal.hide('conversion-form')
+    },
+    beforeOpen (event) {
+      console.log(event.params.coinName)
+      this.coinName = event.params.coinName
     },
     cancelAutoUpdate: function () { clearInterval(this.timer) },
     beforeDestroy () {
@@ -101,4 +119,5 @@ export default {
   }
 }
 </script>
-<style src="../styles/main.css"></style>
+<style src="../styles/table.css"></style>
+<style src="../styles/modal.css"></style>
